f6f60e15cc43d20e46c3de4d0e13eb5b70e716a5
George K <gkwambata@gmail.com>
2025-11-26 14:24:16 +0300

    chore: add items

--- includes/footer.php
+++ includes/footer.php
@@ -51,9 +51,10 @@
                     <ul class="cs_footer_widget_menu">
                         <li><a href="/index">Home</a></li>
                         <li><a href="/about">About</a></li>
-                        <li><a href="/blog">Blog</a></li>
                         <li><a href="/services">Services</a></li>
-                       
+                        <li><a href="/our-impact.php">Our Impact</a></li>
+                        <li><a href="/blog">Blog</a></li>
+                        <li><a href="/work-with-us">Work with Us</a></li>                       
                         <li><a href="contact">Contact</a></li>
                     </ul>
                 </div>
